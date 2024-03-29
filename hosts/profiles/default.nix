a4132c5999052fc6fa4f114317fd0434bc81469c
Daniel Sampliner <samplinerD@gmail.com>
2024-03-29 00:16:26 -0400

    feat(default): add default profile

---
+++ hosts/profiles/default.nix
@@ -0,0 +1,15 @@
+# SPDX-FileCopyrightText: 2024 Daniel Sampliner <samplinerD@gmail.com>
+#
+# SPDX-License-Identifier: GLWTPL
+
+{ lib, ... }:
+{
+  boot = {
+    ephemeral.enable = lib.mkDefault true;
+    # Use the systemd-boot EFI boot loader.
+    loader.systemd-boot.enable = true;
+    loader.efi.canTouchEfiVariables = true;
+  };
+
+  sops.userPasswords.root = ./passwd.sops;
+}
