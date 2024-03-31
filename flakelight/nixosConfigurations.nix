1a3b3ce8922e44961ac2e420b5275cebbbab67d3
Daniel Sampliner <samplinerD@gmail.com>
2024-03-31 19:43:10 -0400

    feat(thiccpad): import nixos-hardware

--- flakelight/nixosConfigurations.nix
+++ flakelight/nixosConfigurations.nix
@@ -17,6 +17,10 @@ let
     default = "x86_64-linux";
   };
 
+  extraModules = {
+    "thiccpad" = [ inputs.nixos-hardware.nixosModules.lenovo-legion-16ach6h-nvidia ];
+  };
+
   hosts = lib.pipe hostsDir [
     builtins.readDir
     (lib.filterAttrs (n: t: n != "profiles" && t == "directory"))
@@ -27,63 +31,66 @@ let
   mkNixosSystem = host: {
     system = hostSystems.${host} or hostSystems.default;
 
-    modules = builtins.attrValues outputs.nixosModules or { } ++ [
-      (_: {
-        nix = {
-          channel.enable = false;
-          nixPath = lib.mapAttrsToList (n: v: "${n}=${v}") inputs;
+    modules =
+      builtins.attrValues outputs.nixosModules or { }
+      ++ extraModules.${host} or [ ]
+      ++ [
+        (_: {
+          nix = {
+            channel.enable = false;
+            nixPath = lib.mapAttrsToList (n: v: "${n}=${v}") inputs;
 
-          registry =
-            let
-              mkFlakeRegistry =
-                name:
-                {
-                  owner ? "nixos",
-                  repo ? null,
-                  type ? "github",
-                }:
-                {
+            registry =
+              let
+                mkFlakeRegistry =
+                  name:
+                  {
+                    owner ? "nixos",
+                    repo ? null,
+                    type ? "github",
+                  }:
+                  {
+                    from = {
+                      id = name;
+                      type = "indirect";
+                    };
+
+                    to = {
+                      inherit owner type;
+                      inherit (inputs.${name}.sourceInfo) rev;
+
+                      repo = if repo != null then repo else name;
+                    };
+                  };
+              in
+              {
+                devshell = mkFlakeRegistry "devshell" { owner = "numtide"; };
+                flake-utils = mkFlakeRegistry "flake-utils" { owner = "nix-community"; };
+                flakelight = mkFlakeRegistry "flakelight" { repo = "nix-community"; };
+                git-hooks = mkFlakeRegistry "git-hooks" { repo = "cachix"; };
+                nixpkgs = mkFlakeRegistry "nixpkgs" { };
+                treefmt-nix = mkFlakeRegistry "treefmt-nix" { repo = "cachix"; };
+                unstable = mkFlakeRegistry "unstable" { repo = "nixpkgs"; };
+              }
+              // lib.mapAttrs' (
+                n: v:
+                lib.nameValuePair "${n}-local" {
+                  flake = v;
                   from = {
-                    id = name;
+                    id = n;
+                    ref = "local";
                     type = "indirect";
                   };
+                }
+              ) inputs;
+          };
 
-                  to = {
-                    inherit owner type;
-                    inherit (inputs.${name}.sourceInfo) rev;
+          nixpkgs.config = config.nixpkgs.config;
+          networking.hostName = builtins.baseNameOf host;
+        })
 
-                    repo = if repo != null then repo else name;
-                  };
-                };
-            in
-            {
-              devshell = mkFlakeRegistry "devshell" { owner = "numtide"; };
-              flake-utils = mkFlakeRegistry "flake-utils" { owner = "nix-community"; };
-              flakelight = mkFlakeRegistry "flakelight" { repo = "nix-community"; };
-              git-hooks = mkFlakeRegistry "git-hooks" { repo = "cachix"; };
-              nixpkgs = mkFlakeRegistry "nixpkgs" { };
-              treefmt-nix = mkFlakeRegistry "treefmt-nix" { repo = "cachix"; };
-              unstable = mkFlakeRegistry "unstable" { repo = "nixpkgs"; };
-            }
-            // lib.mapAttrs' (
-              n: v:
-              lib.nameValuePair "${n}-local" {
-                flake = v;
-                from = {
-                  id = n;
-                  ref = "local";
-                  type = "indirect";
-                };
-              }
-            ) inputs;
-        };
-
-        nixpkgs.config = config.nixpkgs.config;
-        networking.hostName = builtins.baseNameOf host;
-      })
-
-      "${hostsDir}/${host}/configuration.nix"
-    ];
+        "${hostsDir}/${host}/configuration.nix"
+      ];
   };
 in
 builtins.mapAttrs (h: _: mkNixosSystem h) hosts
