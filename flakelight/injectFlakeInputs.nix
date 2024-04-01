97d22c2edf01d49c6784f58e56ac829572cc274a
Daniel Sampliner <samplinerD@gmail.com>
2024-03-31 23:52:28 -0400

    chore(nixosConfigurations): reorganize nix flake registry injection

---
+++ flakelight/injectFlakeInputs.nix
@@ -0,0 +1,47 @@
+# SPDX-FileCopyrightText: 2024 Daniel Sampliner <samplinerD@gmail.com>
+#
+# SPDX-License-Identifier: GLWTPL
+
+{ src }:
+{ inputs, lib, ... }:
+{
+  nix = {
+    channel.enable = false;
+    nixPath = lib.mapAttrsToList (n: v: "${n}=${v}") inputs;
+
+    registry =
+      let
+        urlRefs =
+          let
+            srcInputs = (import "${src}/flake.nix").inputs;
+          in
+          builtins.mapAttrs (name: input: {
+            from = {
+              id = name;
+              type = "indirect";
+            };
+            to =
+              let
+                srcFlakeRef = builtins.parseFlakeRef srcInputs.${name}.url;
+              in
+              {
+                inherit (srcFlakeRef) owner repo type;
+                inherit (input) rev;
+              };
+          }) inputs;
+
+        localRefs = lib.mapAttrs' (
+          n: v:
+          lib.nameValuePair "${n}-local" {
+            flake = v;
+            from = {
+              id = n;
+              ref = "local";
+              type = "indirect";
+            };
+          }
+        ) inputs;
+      in
+      urlRefs // localRefs;
+  };
+}
--- flakelight/nixosConfigurations.nix
+++ flakelight/nixosConfigurations.nix
@@ -35,56 +35,8 @@ let
       builtins.attrValues outputs.nixosModules or { }
       ++ extraModules.${host} or [ ]
       ++ [
+        (import ./injectFlakeInputs.nix { inherit src; })
         (_: {
-          nix = {
-            channel.enable = false;
-            nixPath = lib.mapAttrsToList (n: v: "${n}=${v}") inputs;
-
-            registry =
-              let
-                mkFlakeRegistry =
-                  name:
-                  {
-                    owner ? "nixos",
-                    repo ? null,
-                    type ? "github",
-                  }:
-                  {
-                    from = {
-                      id = name;
-                      type = "indirect";
-                    };
-
-                    to = {
-                      inherit owner type;
-                      inherit (inputs.${name}.sourceInfo) rev;
-
-                      repo = if repo != null then repo else name;
-                    };
-                  };
-              in
-              {
-                devshell = mkFlakeRegistry "devshell" { owner = "numtide"; };
-                flake-utils = mkFlakeRegistry "flake-utils" { owner = "nix-community"; };
-                flakelight = mkFlakeRegistry "flakelight" { repo = "nix-community"; };
-                git-hooks = mkFlakeRegistry "git-hooks" { repo = "cachix"; };
-                nixpkgs = mkFlakeRegistry "nixpkgs" { };
-                treefmt-nix = mkFlakeRegistry "treefmt-nix" { repo = "cachix"; };
-                unstable = mkFlakeRegistry "unstable" { repo = "nixpkgs"; };
-              }
-              // lib.mapAttrs' (
-                n: v:
-                lib.nameValuePair "${n}-local" {
-                  flake = v;
-                  from = {
-                    id = n;
-                    ref = "local";
-                    type = "indirect";
-                  };
-                }
-              ) inputs;
-          };
-
           nixpkgs.config = config.nixpkgs.config;
           networking.hostName = builtins.baseNameOf host;
         })
