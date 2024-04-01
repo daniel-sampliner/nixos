b2931d676a427d2c589fa0245751abac007f14f6
Daniel Sampliner <samplinerD@gmail.com>
2024-04-01 14:42:34 -0400

    feat(svg-term-cli): add package

---
+++ flakelight/packages/svg-term-cli/default.nix
@@ -0,0 +1,49 @@
+# SPDX-FileCopyrightText: 2024 Daniel Sampliner <samplinerD@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+
+{
+  fetchFromGitHub,
+  fetchYarnDeps,
+  lib,
+  mkYarnPackage,
+}:
+let
+  pname = "svg-term-cli";
+  version = "2.1.1";
+
+  src = fetchFromGitHub {
+    owner = "marionebl";
+    repo = pname;
+    rev = "v${version}";
+
+    hash = "sha256-sB4/SM48UmqaYKj6kzfjzITroL0l/QL4Gg5GSrQ+pdk=";
+  };
+in
+mkYarnPackage {
+  inherit pname src version;
+
+  packageJSON = ./package.json;
+
+  offlineCache = fetchYarnDeps {
+    yarnLock = "${src}/yarn.lock";
+    hash = "sha256-4Q1NP3VhnACcrZ1XUFPtgSlk1Eh8Kp02rOgijoRJFcI=";
+  };
+
+  buildPhase = ''
+    runHook preBuild
+
+    export HOME=$PWD/yarn_home
+    yarn --offline build
+
+    runHook postBuild
+  '';
+
+  postInstall = ''
+    bin="$(readlink -e $out/bin/svg-term)"
+    chmod a+x "$bin"
+  '';
+
+  meta.license = lib.licenses.mit;
+  meta.mainProgram = "svg-term";
+}
