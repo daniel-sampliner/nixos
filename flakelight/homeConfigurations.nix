2517577fe9fd521e05cce46e61247d020ba57517
Daniel Sampliner <samplinerD@gmail.com>
2024-04-02 21:13:32 -0400

    feat(homeConfigurations): home-manager scaffolding

---
+++ flakelight/homeConfigurations.nix
@@ -0,0 +1,46 @@
+# SPDX-FileCopyrightText: 2024 Daniel Sampliner <samplinerD@gmail.com>
+#
+# SPDX-License-Identifier: GLWTPL
+
+{
+  config,
+  lib,
+  src,
+  ...
+}:
+let
+  usersDir = "${src}/users";
+
+  homeDirs = { };
+  extraModules = { };
+
+  users = lib.pipe usersDir [
+    builtins.readDir
+    (lib.filterAttrs (n: t: n != "profiles" && t == "directory"))
+    (builtins.mapAttrs (d: _: "${usersDir}/${d}/home.nix"))
+    (lib.filterAttrs (_: f: builtins.pathExists f))
+  ];
+
+  mkHomeConfiguration = system: user: config: {
+    inherit system;
+
+    modules =
+      builtins.attrValues config.homeModules or { }
+      ++ extraModules.${user} or [ ]
+      ++ [
+        (_: { home.homeDirectory = homeDirs.${user} or "/home/${user}"; })
+        config
+      ];
+  };
+
+  mkHomeConfigurations =
+    host: system:
+    lib.mapAttrs' (
+      user: config: lib.nameValuePair "${user}@${host}" (mkHomeConfiguration system user config)
+    ) users;
+in
+lib.pipe config.nixosConfigurations [
+  (builtins.mapAttrs (_: v: v.system))
+  (lib.mapAttrsToList mkHomeConfigurations)
+  (builtins.foldl' (a: b: a // b) { })
+]
