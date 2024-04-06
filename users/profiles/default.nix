1fb0a493975a3ba44d5978024ec29c9f18ac496a
Daniel Sampliner <samplinerD@gmail.com>
2024-04-06 14:15:07 -0400

    feat(default): add shell aliases

--- users/profiles/default.nix
+++ users/profiles/default.nix
@@ -3,6 +3,8 @@
 # SPDX-License-Identifier: GLWTPL
 
 _: {
+  imports = [ ./aliases ];
+
   programs.zsh.envExtra = ''
     typeset -aUT XDG_CONFIG_DIRS xdg_config_dirs
     typeset -aUT XDG_DATA_DIRS xdg_data_dirs
