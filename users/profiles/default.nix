99f92bb535f7fbc4cde5d0154161ac8c76c638e8
Daniel Sampliner <samplinerD@gmail.com>
2024-04-06 14:10:03 -0400

    feat(default): XDG Base Directory Specification

---
+++ users/profiles/default.nix
@@ -0,0 +1,12 @@
+# SPDX-FileCopyrightText: 2024 Daniel Sampliner <samplinerD@gmail.com>
+#
+# SPDX-License-Identifier: GLWTPL
+
+_: {
+  programs.zsh.envExtra = ''
+    typeset -aUT XDG_CONFIG_DIRS xdg_config_dirs
+    typeset -aUT XDG_DATA_DIRS xdg_data_dirs
+  '';
+
+  xdg.enable = true;
+}
