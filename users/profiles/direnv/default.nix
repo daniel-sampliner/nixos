e55d385b217bd1bb5cecc4dc186faf9fc8bb3f00
Daniel Sampliner <samplinerD@gmail.com>
2024-04-07 14:09:46 -0400

    feat(default): enable direnv

---
+++ users/profiles/direnv/default.nix
@@ -0,0 +1,66 @@
+# SPDX-FileCopyrightText: 2024 Daniel Sampliner <samplinerD@gmail.com>
+#
+# SPDX-License-Identifier: GLWTPL
+
+{ lib, pkgs, ... }:
+let
+  direnv-hooks =
+    pkgs.runCommand "direnv-hooks"
+      {
+        nativeBuildInputs = [
+          pkgs.direnv
+          pkgs.zsh
+        ];
+      }
+      ''
+        mkdir -p $out/share/direnv
+        cd $out/share/direnv
+
+        for sh in bash fish zsh; do
+          direnv hook "$sh" >hook."$sh"
+        done
+        zsh -f -c 'zcompile -U hook.zsh'
+      '';
+  sourceHook = shell: ''
+    . "${direnv-hooks}/share/direnv/hook.${shell}"
+  '';
+in
+{
+  home.packages = [ pkgs.pigz ];
+
+  home.sessionVariables.DIRENV_LOG_FORMAT =
+    let
+      ESC = builtins.readFile (pkgs.runCommandLocal "ESC" { } ''echo -ne '\033' >$out'');
+    in
+    "${ESC}[2mdirenv: %s${ESC}[0m";
+
+  programs = {
+    bash.initExtra = lib.mkAfter (sourceHook "bash");
+    fish.interactiveShellInit = lib.mkAfter (sourceHook "fish");
+
+    direnv = {
+      enable = true;
+      enableBashIntegration = false;
+      enableFishIntegration = false;
+      enableZshIntegration = false;
+      stdlib = ''
+        direnv_layout_dir() {
+          echo "''${direnv_layout_dir:-$PWD/.direnv}"
+        }
+      '';
+    };
+
+    git.ignores = [ ".direnv" ];
+
+    zsh.initExtra = ''
+      ${sourceHook "zsh"}
+
+      ${builtins.readFile ./direnv_completion.zsh}
+      autoload -RUz add-zsh-hook
+      add-zsh-hook chpwd _direnv_completion_hook
+      add-zsh-hook precmd _direnv_completion_hook
+    '';
+  };
+
+  xdg.configFile."direnv/lib/nix-direnv.sh".source = "${pkgs.nix-direnv}/share/nix-direnv/direnvrc";
+}
---
+++ users/profiles/direnv/direnv_completion.zsh
@@ -0,0 +1,55 @@
+# SPDX-FileCopyrightText: 2024 Daniel Sampliner <samplinerD@gmail.com>
+#
+# SPDX-License-Identifier: GLWTPL
+
+# shellcheck shell=bash
+
+_direnv_completion_hook() {
+	trap -- "" SIGINT
+	trap 'trap - SIGINT' EXIT
+
+	if ! [[ -v DEVSHELL_DIR || -v _direnv_completion__last_devshell_dir ]]; then
+		return 0
+	fi
+
+	if [[ "${DEVSHELL_DIR:-}" != "${_direnv_completion__last_devshell_dir:-}" ]]; then
+		if [[ -n ${_direnv_completion__last_fpath:-} ]]; then
+			FPATH=${_direnv_completion__last_fpath}
+		fi
+		unset _direnv_completion__last_devshell_dir
+		unset _direnv_completion__last_fpath
+
+		compinit -C
+
+		if [[ ! -v DEVSHELL_DIR ]]; then
+			return 0
+		fi
+	fi
+
+	if ! [[ -d $DEVSHELL_DIR/share/zsh && -v DIRENV_FILE ]]; then
+		return 0
+	fi
+
+	_direnv_completion__set_if_unset _direnv_completion__last_devshell_dir "$DEVSHELL_DIR"
+	_direnv_completion__set_if_unset _direnv_completion__last_fpath "$FPATH"
+
+	local d dd changed=0
+	for d in "functions" "site-functions"; do
+		dd="$DEVSHELL_DIR/share/zsh/$d"
+		[[ ! -d $dd ]] && continue
+		[[ -n ${fpath[(r)$dd]+1} ]] && continue
+		fpath=("$dd" "${fpath[@]}")
+		changed=1
+	done
+
+	((!changed)) && return 0
+
+	compinit -w -d "${DIRENV_FILE%/.envrc}/.direnv/zcompdump"
+	zcompile -U "${DIRENV_FILE%/.envrc}/.direnv/zcompdump"
+}
+
+_direnv_completion__set_if_unset() {
+	if [[ ! -v ${1:?} ]]; then
+		printf -v "$1" '%s' "${2:?}"
+	fi
+}
