8520845d61bed2b7347398089259e3766e533ebf
Daniel Sampliner <samplinerD@gmail.com>
2024-04-06 23:43:07 -0400

    feat(zsh): basic scaffolding

---
+++ users/profiles/zsh/default.nix
@@ -0,0 +1,23 @@
+# SPDX-FileCopyrightText: 2024 Daniel Sampliner <samplinerD@gmail.com>
+#
+# SPDX-License-Identifier: GLWTPL
+
+{ config, lib, ... }:
+{
+  imports = [ ./zprof.nix ];
+
+  programs.zsh =
+    let
+      inherit (config.home) homeDirectory;
+      dotDir = "${lib.removePrefix "${homeDirectory}/" config.xdg.configHome}/zsh";
+    in
+    {
+      inherit dotDir;
+
+      enable = true;
+
+      envExtra = ''
+        setopt NO_GLOBAL_RCS
+      '';
+    };
+}
