ca60a9ba78c1f7fb235857c1c9c53edf18b5e074
Daniel Sampliner <samplinerD@gmail.com>
2024-04-09 18:41:25 -0400

    feat(v4l2loopback): add profile

---
+++ hosts/profiles/v4l2loopback.nix
@@ -0,0 +1,31 @@
+# SPDX-FileCopyrightText: 2024 Daniel Sampliner <samplinerD@gmail.com>
+#
+# SPDX-License-Identifier: GLWTPL
+
+{ config, pkgs, ... }:
+let
+  pkg = config.boot.kernelPackages.v4l2loopback;
+in
+{
+
+  boot.extraModprobeConfig = ''
+    options v4l2loopback exclusive_caps=1 card_label="virtual_webcam"
+  '';
+
+  boot.extraModulePackages = [ pkg ];
+  environment.systemPackages = [ pkg ];
+
+  services.udev.packages =
+    let
+      v4l2loopback-rules = pkgs.runCommand "v4l2loopback-rules" { } ''
+        install -D -m 0644 -t "$out/lib/udev/rules.d" "${pkg.src}/udev/"*.rules
+        for r in "$out/lib/udev/rules.d/"*; do
+          substituteInPlace "$r" \
+            --replace "/usr/bin/find " "${pkgs.findutils}/bin/find " \
+            --replace "/bin/chgrp " "${pkgs.coreutils}/bin/chgrp " \
+            --replace "/bin/chmod " "${pkgs.coreutils}/bin/chmod "
+        done
+      '';
+    in
+    [ v4l2loopback-rules ];
+}
