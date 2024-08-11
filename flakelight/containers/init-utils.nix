c841e45539e3a51db12af7a795e066f615e96473
Daniel Sampliner <samplinerD@gmail.com>
2024-08-10 23:17:44 -0400

    feat(init-utils): add network tools

--- flakelight/containers/init-utils.nix
+++ flakelight/containers/init-utils.nix
@@ -11,29 +11,46 @@
   coreutils,
   dash,
   execline,
+  iproute2,
+  iptables-legacy,
+  procps,
+  s6,
+  s6-portable-utils,
   sops,
+  wireguard-tools,
 }:
+let
+  my-iproute2 = iproute2.override { iptables = iptables-legacy; };
+  my-wireguard-tools = wireguard-tools.override {
+    iproute2 = my-iproute2;
+    iptables = iptables-legacy;
+    procps = procps.override { withSystemd = false; };
+  };
+in
 nix2container.buildImage {
   name = "init-utils";
-  tag = "0.0.1";
+  tag = "0.0.2";
 
-  copyToRoot = [
-    (buildEnv {
-      name = "root";
-      paths = [
-        coreutils
-        dash
-        execline
-        sops
+  copyToRoot = buildEnv {
+    name = "root";
+    paths = [
+      coreutils
+      dash
+      execline
+      s6
+      s6-portable-utils
+      sops
 
-        (runCommandLocal "dashSymlink" { } ''
-          mkdir -p $out/bin
-          ln -sf ${lib.getExe dash} $out/bin/sh
-        '')
-      ];
-      pathsToLink = [ "/bin" ];
-    })
-  ];
+      my-iproute2
+      my-wireguard-tools
+
+      (runCommandLocal "dashSymlink" { } ''
+        mkdir -p $out/bin
+        ln -sf ${lib.getExe dash} $out/bin/sh
+      '')
+    ];
+    pathsToLink = [ "/bin" ];
+  };
 
   config = {
     Entrypoint = [
