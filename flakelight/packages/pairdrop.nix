8ccb8173f08b4a9e7491cbcc0628b63f508a4760
Daniel Sampliner <samplinerD@gmail.com>
2024-10-07 22:52:22 -0400

    feat(pairdrop): bump to v1.10.10

---
+++ flakelight/packages/pairdrop.nix
@@ -0,0 +1,36 @@
+# SPDX-FileCopyrightText: 2024 Daniel Sampliner <samplinerD@gmail.com>
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+{
+  buildNpmPackage,
+  fetchFromGitHub,
+
+  pairdrop,
+}:
+let
+  version = "1.10.10";
+in
+(pairdrop.override {
+  buildNpmPackage =
+    args:
+    buildNpmPackage (
+      args
+      // {
+        src = fetchFromGitHub {
+          owner = "schlagmichdoch";
+          repo = "PairDrop";
+          rev = "v${version}";
+          hash = "sha256-urkCLZ6Vwje/F9f+QZswFigzYYVUkG5I4UmO1FmBaU0=";
+        };
+        npmDepsHash = "sha256-n19pqG8gHRaFH3GnKfyhqq7U1EdQUlzxeXrrQY8Fkf0=";
+      }
+    );
+}).overrideAttrs
+  (prev: {
+    inherit version;
+
+    installPhase = builtins.replaceStrings [ "index.js" ] [ "server/index.js" ] (
+      prev.installPhase or ""
+    );
+  })
