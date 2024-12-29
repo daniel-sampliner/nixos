fd006e1c3c67dc6f55a9822bba4f089159a7795a
Daniel Sampliner <samplinerD@gmail.com>
2024-12-29 16:50:29 -0500

    feat(git-hooks): move out hook-specific gitignores

--- flakelight/checks/git-hooks.nix
+++ flakelight/checks/git-hooks.nix
@@ -3,22 +3,40 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
 pkgs:
-pkgs.inputs'.git-hooks.lib.run {
-  inherit (pkgs) src;
+let
+  inherit (pkgs.inputs') git-hooks;
+  run = git-hooks.lib.run {
+    inherit (pkgs) src;
 
-  hooks = {
-    commitizen.enable = true;
+    hooks = {
+      commitizen.enable = true;
 
-    deadnix.enable = true;
-    deadnix.settings.edit = true;
+      deadnix.enable = true;
+      deadnix.settings.edit = true;
 
-    editorconfig-checker.enable = true;
-    nil.enable = true;
-    shellcheck.enable = true;
-    statix.enable = true;
-    end-of-file-fixer.enable = true;
-    reuse.enable = true;
-    treefmt.enable = true;
-    treefmt.package = pkgs.outputs'.formatter;
+      editorconfig-checker.enable = true;
+      nil.enable = true;
+      shellcheck.enable = true;
+      statix.enable = true;
+      end-of-file-fixer.enable = true;
+      reuse.enable = true;
+
+      treefmt.enable = true;
+      treefmt.package = pkgs.outputs'.formatter;
+    };
   };
+in
+run.overrideAttrs (prev: {
+  buildPhase =
+    ''
+      {
+        echo .cache/pre-commit
+        echo .gitconfig
+      } >>.gitignore
+    ''
+    + prev.buildPhase;
+})
+// {
+  inherit (run) config shellHook;
+  inherit (run.config) enabledPackages;
 }
