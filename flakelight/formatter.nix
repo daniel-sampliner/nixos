011e9517a415d520395cc66434d03c98a08ca18d
Daniel Sampliner <samplinerD@gmail.com>
2024-12-29 16:43:28 -0500

    feat(formatter): include moduleArgs

--- flakelight/formatter.nix
+++ flakelight/formatter.nix
@@ -3,37 +3,43 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
 pkgs:
-pkgs.inputs.treefmt-nix.lib.mkWrapper pkgs {
-  projectRootFile = "flake.nix";
+let
+  inherit (pkgs) lib;
+  inherit (pkgs.inputs) treefmt-nix;
 
-  programs = {
-    nixfmt.enable = true;
+  mod = treefmt-nix.lib.evalModule pkgs {
+    projectRootFile = "flake.nix";
 
-    shfmt.enable = true;
-    shfmt.indent_size = null;
+    programs = {
+      nixfmt.enable = true;
 
-    taplo.enable = true;
+      shfmt.enable = true;
+      shfmt.indent_size = null;
 
-    yamlfmt.enable = true;
-  };
+      taplo.enable = true;
 
-  settings.formatter = {
-    nixfmt.excludes = [ "hardware-configuration.nix" ];
+      yamlfmt.enable = true;
+    };
 
-    yamlfmt.options =
-      let
-        cfg = pkgs.writers.writeYAML "yamlfmt.yaml" {
-          formatter = {
-            type = "basic";
-            retain_line_breaks_single = true;
-            scan_folded_as_literal = true;
-            trim_trailing_whitespace = true;
+    settings.formatter = {
+      nixfmt.excludes = [ "hardware-configuration.nix" ];
+
+      yamlfmt.options =
+        let
+          cfg = pkgs.writers.writeYAML "yamlfmt.yaml" {
+            formatter = {
+              type = "basic";
+              retain_line_breaks_single = true;
+              scan_folded_as_literal = true;
+              trim_trailing_whitespace = true;
+            };
           };
-        };
-      in
-      [
-        "-conf"
-        cfg.outPath
-      ];
+        in
+        [
+          "-conf"
+          cfg.outPath
+        ];
+    };
   };
-}
+in
+lib.attrsets.recursiveUpdate mod.config.build.wrapper { passthru.moduleArgs = mod; }
