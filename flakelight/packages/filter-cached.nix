76614b2022a363f21c6c0d57fcfcbbf55e598cff
Daniel Sampliner <samplinerD@gmail.com>
2024-12-30 20:49:02 -0500

    build(gh/build): filter cached builds in gen-matrix

---
+++ flakelight/packages/filter-cached.nix
@@ -0,0 +1,13 @@
+# SPDX-FileCopyrightText: 2024 Daniel Sampliner <samplinerD@gmail.com>
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+{
+  writers,
+}:
+writers.writeExecline { flags = "-WS1"; } "/bin/filter-cached" ''
+  forstdin -E -p attr
+  if -Xnt { redirfd -w 1 /dev/null nix path-info --eval-store auto --store https://cache.nixos.org $attr }
+  if -Xnt { redirfd -w 1 /dev/null nix path-info --eval-store auto --store $1 $attr }
+  echo $attr
+''
