cfe85a97d2d4a83899266b7d71480c7700290ca4
Daniel Sampliner <samplinerD@gmail.com>
2025-02-20 14:09:24 -0500

    fixup! WIP SYSTEMD-SOPS

--- packages/systemd-sops/default.nix
+++ packages/systemd-sops/default.nix
@@ -8,14 +8,14 @@ stdenv.mkDerivation {
   pname = "systemd-sops";
   version = "0.0.1";
 
-  src = builtins.path { name = "systemd-sops"; path = ./.; };
-    # let
-    #   fs = lib.fileset.toSource {
-    #     root = builtins.path { path = ./.; name = "systemd-sops"; };
-    #     fileset = lib.fileset.difference ./. ./default.nix;
-    #   };
-    # in
-    # fs;
+  src =
+    let
+      fs = lib.fileset.toSource {
+        root = ./.;
+        fileset = lib.fileset.difference ./. ./default.nix;
+      };
+    in
+    fs;
 
   nativeBuildInputs = [ zig.hook ];
 }
