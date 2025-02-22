ce3dea5e0757ce2b4d8d52e465771139a3f9f65d
Daniel Sampliner <samplinerD@gmail.com>
2025-02-22 01:48:43 -0500

    fixup! WIP SYSTEMD-SOPS

--- packages/systemd-sops/src/main.zig
+++ packages/systemd-sops/src/main.zig
@@ -43,38 +43,29 @@ pub fn syslogFn(
     std.debug.lockStdErr();
     defer std.debug.unlockStdErr();
     nosuspend {
-        writer.print(level_txt ++ prefix2 ++ format ++ "", args) catch return;
+        writer.print(level_txt ++ prefix2 ++ format ++ "\n", args) catch return;
         bw.flush() catch return;
     }
 }
 
 pub fn main() !void {
-    // const sops_base_dir: [:0]const u8 = try (std.posix.getenv("SOPS_BASE_DIR") orelse blk: {
-    //     std.log.err("Missing required environment variable SOPS_BASE_DIR", .{});
-    //     break :blk error.InvalidConfiguration;
-    // });
-    // std.log.debug("sops_base_dir: {s}", .{sops_base_dir});
+    var sops_file_idx: usize = 0;
+    var sops_file: [std.fs.MAX_PATH_BYTES:0]u8 = undefined;
+    @memset(&sops_file, 0);
 
-    var buffer: [@sizeOf(std.posix.sockaddr.un)]u8 = undefined;
-    const cred_info = try credInfoFromSocket(0, &buffer);
-    std.log.debug("cred_info: {s}", .{cred_info});
-
-    {
-        const writer = std.io.getStdOut().writer();
-        try writer.print("{s}", .{cred_info});
+    if (std.posix.getenv("SOPS_BASE_DIR")) |sops_base_dir| {
+        sops_file_idx = sops_base_dir.len;
+        @memcpy(sops_file[0..sops_file_idx], sops_base_dir.ptr);
+    } else {
+        std.log.err("Missing required environment variable SOPS_BASE_DIR", .{});
+        return error.InvalidConfiguration;
     }
-    // var sops_file: [std.fs.MAX_PATH_BYTES:0]u8 = undefined;
-    // try (socketToSOPS(&sops_file, 0))
+    std.log.debug("sops_file: {s}", .{sops_file});
 
-    // var cmd = [_:null]?[*:0]const u8{ "echo".ptr, key[0..:0].ptr  };
-    // cmd = cmd;
-    // std.log.debug("{any}", .{cmd});
+    _ = try credInfoFromSocket(0, sops_file[sops_file_idx..]);
+    std.log.debug("sops_file: {s}", .{sops_file});
 
-    // var args = [_:null]?[*:0]const u8{ "hello", "goodbye", null };
-    // const env = [_:null]?[*:0]u8{null};
-    // return std.posix.execvpeZ("echo", args, env);
-    // const writer = std.io.getStdOut().writer();
-    // try writer.print("{s}", .{key});
+    return execSops(&sops_file, .{ .command = "echo" });
 }
 
 fn credInfoFromSocket(fd: std.posix.socket_t, buffer: []u8) ![]u8 {
@@ -86,8 +77,15 @@ fn credInfoFromSocket(fd: std.posix.socket_t, buffer: []u8) ![]u8 {
     try std.posix.getpeername(fd, @ptrCast(&addr), &addr_len);
 
     if (addr.family != std.posix.AF.UNIX) {
+        switch (addr.family) {
+            std.posix.AF.INET => log.err("fd {} is INET socket", .{fd}),
+            std.posix.AF.INET6 => log.err("fd {} is INET6 socket", .{fd}),
+            else => log.err("fd {} is {} socket", .{ fd, addr.family }),
+        }
+
         return error.SocketNotUnix;
     }
+
     if (addr.path[0] != 0) {
         return error.UnixSocketNotAbstract;
     }
@@ -148,85 +146,81 @@ test "credInfoFromSocket" {
     try std.testing.expectEqualStrings("/" ++ cred_info, cred_info_got);
 }
 
-test "sopsFile" {
-    const log = std.log.scoped(.sopsFile);
+fn execSops(
+    sops_file: [:0]const u8,
+    options: struct { command: [:0]const u8 = "sops" },
+) anyerror {
+    const log = std.log.scoped(.execSops);
 
-    const sops_base_dir: [:0]const u8 = "/sops";
-    const cred_info: []u8 = blk: {
-        const name = "/service/key";
-        var array: [name.len]u8 = name.*;
-        break :blk &array;
+    const argv = [_:null]?[*:0]const u8{ options.command.ptr, "--decrypt".ptr, sops_file.ptr };
+    log.debug("argv: {any}", .{argv});
+    for (argv, 0..) |arg, idx| {
+        std.log.debug("  {}: {?s}", .{ idx, arg });
+    }
+
+    const sops_age_key_file_env = "SOPS_AGE_KEY_FILE";
+    var buffer: [std.fs.MAX_PATH_BYTES + sops_age_key_file_env.len + 1:0]u8 = undefined;
+    @memset(&buffer, 0);
+    @memcpy(buffer[0..sops_age_key_file_env.len], sops_age_key_file_env.ptr);
+    buffer[sops_age_key_file_env.len] = '=';
+
+    const envp = [_:null]?[*:0]const u8{
+        if (std.posix.getenv(sops_age_key_file_env)) |e| blk: {
+            @memcpy(buffer[sops_age_key_file_env.len + 1 ..][0..e.len], e);
+            break :blk std.mem.sliceTo(&buffer, 0);
+        } else null,
+
+        null,
     };
-    log.debug("sops_base_dir: {s}", .{sops_base_dir});
-    log.debug("cred_info: {s}", .{cred_info});
+
+    std.log.debug("envp: {any}", .{envp});
+    for (envp, 0..) |env, idx| {
+        log.debug("  {}: {?s}", .{ idx, env });
+    }
+
+    const err = std.posix.execvpeZ(argv[0].?, &argv, &envp);
+
+    log.err("failed to execvpeZ '{?s}'", .{argv[0]});
+    return err;
+}
+
+test "execSops" {
+    const log = std.log.scoped(.execSops);
 
     var sops_file: [std.fs.MAX_PATH_BYTES:0]u8 = undefined;
     @memset(&sops_file, 0);
     {
-        var begin_idx: usize = 0;
-        var end_idx: usize = sops_base_dir.len;
-        @memcpy(sops_file[begin_idx..end_idx], sops_base_dir);
-
-        begin_idx = end_idx;
-        end_idx = begin_idx + cred_info.len;
-        @memcpy(sops_file[begin_idx..end_idx], cred_info);
+        const s = "/sops/service/key";
+        @memcpy(sops_file[0..s.len], s);
     }
-
-    log.debug("sops_file: {s} {any}", .{ sops_file, sops_file[0..32] });
 
     const read_fd, const write_fd = try std.posix.pipe();
     const fork_pid = try std.posix.fork();
 
     if (fork_pid == 0) {
-        const argv = [_:null]?[*:0]const u8{ "echo".ptr, &sops_file };
-        const envp = [_:null]?[*:0]const u8{null};
         try std.posix.dup2(write_fd, 1);
         std.posix.close(read_fd);
-        const ret = std.posix.execvpeZ(argv[0].?, &argv, &envp);
-        log.err("failed to execvpeZ: {any}", .{ret});
+        std.posix.close(write_fd);
+        execSops(&sops_file, .{ .command = "echo" }) catch {};
+
         std.process.exit(1);
     }
 
     std.posix.close(write_fd);
+    defer std.posix.close(read_fd);
 
     const reader = (std.fs.File{ .handle = read_fd }).reader();
 
     var buffer: [1024]u8 = undefined;
     var fbs = std.io.fixedBufferStream(&buffer);
-    try reader.streamUntilDelimiter(fbs.writer(), '\n', buffer.len);
-    const stdout = fbs.getWritten();
-
-    const ret = std.posix.waitpid(fork_pid, 0);
-
-    try std.testing.expectEqual(0, ret.status);
-    try std.testing.expectEqualStrings(std.mem.sliceTo(&sops_file, 0), stdout);
-}
-
-fn sopsFile(buf: [:0]u8, socket_path: []const u8, sops_base_dir: []const u8) !void {
-    const key = std.fs.path.basename(socket_path);
-    const unit = try if (std.fs.path.dirname(socket_path)) |dir|
-        std.fs.path.basename(dir)
-    else blk: {
-        std.log.err("Could not parse unit from socket path {s}", .{socket_path});
-        break :blk error.CantParseUnit;
+    reader.streamUntilDelimiter(fbs.writer(), '\n', buffer.len) catch |err| switch (err) {
+        error.EndOfStream => {},
+        else => log.err("failed to read subprocess stdout: {}", .{err}),
     };
 
-    var begin: usize = 0;
-    var end: usize = sops_base_dir.len;
-    @memcpy(buf[begin..end], sops_base_dir);
-    buf[end] = '/';
-    end += 1;
+    const ret = std.posix.waitpid(fork_pid, 0);
+    try std.testing.expectEqual(0, ret.status);
 
-    begin = end;
-    end += unit.len;
-    @memcpy(buf[begin..end], unit);
-    buf[end] = '/';
-    end += 1;
-
-    begin = end;
-    end += key.len;
-    @memcpy(buf[begin..end], key);
-
-    buf[end] = 0;
-    return;
+    const stdout = fbs.getWritten();
+    try std.testing.expectEqualStrings(std.mem.sliceTo(&sops_file, 0), stdout);
 }
