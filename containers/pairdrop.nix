4cbe490a3fcfad8d279223d5c51c69aa46dd6c6d
Daniel Sampliner <samplinerD@gmail.com>
2025-05-07 21:40:19 -0400

    feat(pairdrop): add container

---
+++ containers/pairdrop.nix
@@ -0,0 +1,42 @@
+# SPDX-FileCopyrightText: 2024 Daniel Sampliner <samplinerD@gmail.com>
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+{
+  buildEnv,
+  nix2container,
+  writers,
+
+  curl,
+  pairdrop,
+}:
+nix2container.buildImage {
+  name = pairdrop.pname;
+  tag = pairdrop.version;
+
+  copyToRoot = [
+    (buildEnv {
+      name = "root";
+      paths = [
+        curl
+        pairdrop
+
+        (writers.writeExecline { } "/bin/healthcheck" ''
+          curl -qsSf -o /dev/null -w '%{http_code}\n' localhost:3000
+        '')
+      ];
+    })
+  ];
+
+  config = {
+    Entrypoint = [ "pairdrop" ];
+
+    Env = [
+      "RATE_LIMIT=1"
+      "IPV6_LOCALIZE=4"
+      "WS_FALLBACK=true"
+    ];
+
+    ExposedPorts."3000/tcp" = { };
+  };
+}
