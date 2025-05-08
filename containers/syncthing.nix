77d29a136f538496820a392d0f97c3b43bb4733b
Daniel Sampliner <samplinerD@gmail.com>
2025-05-07 21:40:19 -0400

    feat(syncthing): add container

---
+++ containers/syncthing.nix
@@ -0,0 +1,59 @@
+# SPDX-FileCopyrightText: 2024 Daniel Sampliner <samplinerD@gmail.com>
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+{
+  buildEnv,
+  dockerTools,
+  nix2container,
+  writers,
+
+  curl,
+  syncthing,
+}:
+nix2container.buildImage {
+  name = syncthing.pname;
+  tag = syncthing.version;
+
+  copyToRoot = [
+    dockerTools.caCertificates
+
+    (buildEnv {
+      name = "root";
+      paths = [
+        curl
+        syncthing
+
+        (writers.writeExecline { } "/bin/healthcheck" ''
+          importas -s CURL_ARGS CURL_ARGS
+          curl -qfsS $CURL_ARGS http://localhost:8384/rest/noauth/health
+        '')
+      ];
+      pathsToLink = [ "/bin" ];
+    })
+  ];
+
+  config = {
+    Entrypoint = [
+      "syncthing"
+      "serve"
+    ];
+    Cmd = [ "--no-browser" ];
+
+    Env = [
+      "STCONFDIR=/config"
+      "STDATADIR=/data"
+      "STNODEFAULTFOLDER=true"
+      "STNORESTART=true"
+    ];
+    ExposedPorts = {
+      "21027/udp" = { };
+      "22000/tcp" = { };
+      "22000/udp" = { };
+    };
+    Volumes = {
+      "/config" = { };
+      "/data" = { };
+    };
+  };
+}
