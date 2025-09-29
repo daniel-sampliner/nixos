eb3dd298d5c83431725305c9ec08857e6d4b3c4a
Daniel Sampliner <samplinerD@gmail.com>
2025-09-29 17:57:41 -0400

    feat(devShell): make more minimal

--- devShell.nix
+++ devShell.nix
@@ -2,24 +2,69 @@
 #
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
-_: {
+{ lib, ... }:
+{
   perSystem =
-    { config, pkgs, ... }:
     {
-      devShells.default = pkgs.mkShellNoCC {
-        name = (import ./flake.nix).description;
-        inputsFrom = [ config.treefmt.build.devShell ];
+      config,
+      pkgs,
+      system,
+      ...
+    }:
+    {
+      devShells.default =
+        let
+          mkShellMinimal =
+            {
+              name,
+              packages ? [ ],
+              inputsFrom ? [ ],
+            }:
+            let
+              allPackages =
+                lib.attrsets.catAttrs "nativeBuildInputs" inputsFrom
+                |> lib.lists.flatten
+                |> lib.subtractLists inputsFrom
+                |> (p: packages ++ p);
 
-        packages = builtins.attrValues {
-          inherit (pkgs)
-            home-manager
-            nix-output-monitor
-            ;
+            in
+            builtins.derivation {
+              inherit name system;
 
-          inherit (pkgs.pkgsUnstable)
-            reuse
-            ;
+              builder = lib.meta.getExe pkgs.bash;
+              outputs = [ "out" ];
+
+              XDG_DATA_DIRS =
+                builtins.map (p: p + "/share") allPackages
+                |> builtins.filter lib.filesystem.pathIsDirectory
+                |> builtins.concatStringsSep ":";
+
+              stdenv = pkgs.writeTextDir "setup" ''
+                PATH="${lib.strings.makeBinPath allPackages}"
+                export PATH
+
+                export XDG_DATA_DIRS
+              '';
+
+              args = [
+                (pkgs.writeText "builder.sh" (pkgs.mkShellNoCC { }).buildPhase)
+              ];
+            };
+        in
+        mkShellMinimal {
+          name = (import ./flake.nix).description;
+          inputsFrom = [ config.treefmt.build.devShell ];
+
+          packages = builtins.attrValues {
+            inherit (pkgs)
+              home-manager
+              nix-output-monitor
+              ;
+
+            inherit (pkgs.pkgsUnstable)
+              reuse
+              ;
+          };
         };
-      };
     };
 }
