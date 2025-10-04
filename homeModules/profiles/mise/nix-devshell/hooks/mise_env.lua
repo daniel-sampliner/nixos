c91a92e009421c8a226b7ba3700324254a744182
Daniel Sampliner <samplinerD@gmail.com>
2025-10-04 19:33:59 -0400

    feat(hm/mise): explicit allow env vars

--- homeModules/profiles/mise/nix-devshell/hooks/mise_env.lua
+++ homeModules/profiles/mise/nix-devshell/hooks/mise_env.lua
@@ -7,68 +7,19 @@ local strings = require("strings")
 local log = require("log")
 local nix = require("nix")
 
-local discard_vars = {
-	["CONFIG_SHELL"] = true,
-	["DETERMINISTIC_BUILD"] = true,
-	["DETERMINITSTIC_BUILD"] = true,
-	["HOME"] = true,
-	["HOST_PATH"] = true,
-	["NIX_BUILD_CORES"] = true,
-	["NIX_BUILD_TOP"] = true,
-	["NIX_CFLAGS_COMPILE"] = true,
-	["NIX_ENFORCE_NO_NATIVE"] = true,
-	["NIX_ENFORCE_PURITY"] = true,
-	["NIX_LDFLAGS"] = true,
-	["NIX_LOG_FD"] = true,
-	["NIX_STORE"] = true,
-	["OLDPWD"] = true,
-	["PATH"] = true,
-	["PYTHONHASHSEED"] = true,
-	["PYTHONNOUSERSITE"] = true,
-	["SHELL"] = true,
-	["SOURCE_DATE_EPOCH"] = true,
-	["TEMP"] = true,
-	["TEMPDIR"] = true,
-	["TERM"] = true,
-	["TMP"] = true,
-	["TMPDIR"] = true,
-	["TZ"] = true,
-	["_PYTHON_HOST_PLATFORM"] = true,
-	["_PYTHON_SYSCONFIGDATA_NAME"] = true,
-	["__structuredAttrs"] = true,
-	["buildInputs"] = true,
-	["buildPhase"] = true,
-	["builder"] = true,
-	["cmakeFlags"] = true,
-	["configureFlags"] = true,
-	["depsBuildBuild"] = true,
-	["depsBuildBuildPropagated"] = true,
-	["depsBuildTarget"] = true,
-	["depsBuildTargetPropagated"] = true,
-	["depsHostHost"] = true,
-	["depsHostHostPropagated"] = true,
-	["depsTargetTarget"] = true,
-	["depsTargetTargetPropagated"] = true,
-	["doCheck"] = true,
-	["doInstallCheck"] = true,
-	["dontAddDisableDepTrack"] = true,
-	["mesonFlags"] = true,
-	["nativeBuildInputs"] = true,
-	["out"] = true,
-	["outputs"] = true,
-	["patches"] = true,
-	["phases"] = true,
-	["preferLocalBuild"] = true,
-	["propagatedBuildInputs"] = true,
-	["propagatedNativeBuildInputs"] = true,
-	["shell"] = true,
-	["shellHook"] = true,
-	["stdenv"] = true,
-	["strictDeps"] = true,
-	["system"] = true,
-}
+local keep_vars = {}
+for _, v in ipairs({
+	"IN_NIX_SHELL",
+	"name",
+	"PKG_CONFIG_PATH",
+	"PYTHONPATH",
+	"XDG_DATA_DIRS",
+}) do
+	keep_vars[v] = true
+end
 
 local pathlike_vars = {
+	["PKG_CONFIG_PATH"] = true,
 	["PYTHONPATH"] = true,
 	["XDG_CONFIG_DIRS"] = true,
 	["XDG_DATA_DIRS"] = true,
@@ -83,10 +34,10 @@ function PLUGIN:MiseEnv(ctx)
 	local val
 	local ret = {}
 	for key, value in pairs(env) do
-		if not discard_vars[key] then
+		if keep_vars[key] then
 			if pathlike_vars[key] then
 				val = os.getenv(key)
-				if val then
+				if val and not strings.has_prefix(val, value) then
 					value = strings.join({ value, val }, ":")
 				end
 			end
