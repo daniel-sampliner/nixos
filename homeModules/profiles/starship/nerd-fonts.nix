c66e0b4adce572f9d142231114eba7cadb665b36
Daniel Sampliner <samplinerD@gmail.com>
2025-11-05 14:27:24 -0500

    fix(hm/starship): glyph spacing

--- homeModules/profiles/starship/nerd-fonts.nix
+++ homeModules/profiles/starship/nerd-fonts.nix
@@ -12,13 +12,15 @@ let
   cfg = config.programs.starship;
 in
 {
-  programs.starship.settings = lib.mkMerge [
-    (lib.importTOML "${cfg.package}/share/starship/presets/nerd-font-symbols.toml")
-
-    {
-      custom.continuation.symbol = ": ";
-      directory.truncation_symbol = "…/";
-      shlvl.symbol = "";
-    }
-  ];
+  programs.starship.settings =
+    lib.attrsets.recursiveUpdate
+      (lib.importTOML "${cfg.package}/share/starship/presets/nerd-font-symbols.toml")
+      {
+        aws.symbol = " ";
+        custom.continuation.symbol = ": ";
+        directory.truncation_symbol = "…/";
+        directory.read_only = " 󰌾 ";
+        gcloud.symbol = " ";
+        shlvl.symbol = "";
+      };
 }
