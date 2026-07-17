9b5061bbed6191df14e34b8f4d00270d892edb25
Daniel Sampliner <samplinerD@gmail.com>
2026-07-17 11:10:14 -0400

    fix(git-hooks): patch commitizen to build
    Needed until PR merged:
    
    https://github.com/NixOS/nixpkgs/pull/539725

--- checks/git-hooks.nix
+++ checks/git-hooks.nix
@@ -1,15 +1,42 @@
-# SPDX-FileCopyrightText: 2024 Daniel Sampliner <samplinerD@gmail.com>
+# SPDX-FileCopyrightText: 2024, 2026 Daniel Sampliner <samplinerD@gmail.com>
 #
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
 pkgs:
 let
+  inherit (pkgs) lib;
   inherit (pkgs.inputs') git-hooks;
+
   run = git-hooks.lib.run {
     inherit (pkgs) src;
 
     hooks = {
       commitizen.enable = true;
+      commitizen.package =
+        let
+          exposedPkg = git-hooks.exposed.tools.commitizen;
+          pkg =
+            assert lib.strings.versionOlder (lib.strings.getVersion exposedPkg) "4.16.3";
+            exposedPkg.overrideAttrs (prev: {
+              patches = prev.patches or [ ] ++ [
+                (pkgs.fetchpatch {
+                  url = "https://github.com/commitizen-tools/commitizen/commit/8e9af1ebc9e8065f9062d03af6e09c300fb3519f.patch";
+                  hash = "sha256-gT+CaS8/QLZCSFgs3dOhfcMnUC92a9iG3Ythci3+E9M=";
+                })
+
+                (pkgs.fetchpatch {
+                  url = "https://github.com/commitizen-tools/commitizen/commit/1090d66646f53da1afa7520163d2adad98f28220.patch";
+                  hash = "sha256-iHvdxlmJ+sNUScOOID0ZRAlS5rHatpgMW9jMN4KXRUw=";
+                })
+
+                (pkgs.fetchpatch {
+                  url = "https://github.com/commitizen-tools/commitizen/commit/2b4707cf04929810d8137f12726abe44e67a220e.patch";
+                  hash = "sha256-py7yxVr6ra7WcXYKleebwGjaoJ9yy+KCWrxqrmu0Tlg=";
+                })
+              ];
+            });
+        in
+        pkg;
 
       deadnix.enable = true;
       deadnix.settings.edit = true;
